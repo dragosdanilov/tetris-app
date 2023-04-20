1c05c8b4b9f2406db718bf828b03e0d157a5ad5f
Dragos Danilov <dragosdanilov266@gmail.com>
2023-04-20 16:55:17 -0400

    Implement start/pause functionality for start button using timerId

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const startButton = document.querySelector('#start-button');
     const width = 10;
     let nextRandom = 0;
+    let timerId;
 
     // The Tetrominoes
     const lTetromino = [
@@ -67,7 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // make the tetromino move down every second
-    timerId = setInterval(moveDown, 1000)
+    // timerId = setInterval(moveDown, 1000)
 
     // assign functions to key codes
     function control(e) {
@@ -176,5 +177,18 @@ document.addEventListener('DOMContentLoaded', () => {
             displaySquares[displayIndex + index].classList.add('tetromino')
         })
     }
+
+    // add functionality to the button
+    startButton.addEventListener('click', () => {
+        if (timerId) {
+            clearInterval(timerId)
+            timerId = null;
+        } else {
+            draw()
+            timerId = setInterval(moveDown, 1000)
+            nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+            displayShape()
+        }
+    })
 
 })
